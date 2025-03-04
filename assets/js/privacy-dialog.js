037c82f2481264a0f26dba2ec14f2f802d52d60c
酸柠檬猹/SourLemonJuice <SourLemonJuice233@outlook.com>
2025-03-05 06:27:30 +0800

    实现隐私信息对话框功能，拥有基本的自定义能力和美观性哦

---
+++ assets/js/privacy-dialog.js
@@ -0,0 +1,34 @@
+function showPrivacyPolicyPopup() {
+  // check local storage
+  const popupState = localStorage.getItem("allowPrivacyPolicy");
+  switch (popupState) {
+    case null:
+      break;
+    case "accept":
+      return;
+    default:
+      console.warn(
+        `value unexpected value of local storage allowPrivacyPolicy: ${popupState}`
+      );
+      break;
+  }
+
+  // display dialog
+  const dialog = document.getElementById("privacyPolicyDialog");
+  dialog.show();
+
+  dialog.addEventListener("close", () => {
+    switch (dialog.returnValue) {
+      case "accept":
+        localStorage.setItem("allowPrivacyPolicy", "accept");
+        break;
+      default:
+        console.error(
+          `unexpected value of privacy popup dialog returnValue: ${popup.returnValue}`
+        );
+        return;
+    }
+  });
+}
+
+showPrivacyPolicyPopup();
